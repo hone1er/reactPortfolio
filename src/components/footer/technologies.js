17cc6781dfc64ea00426b283904bf6aaf37ae626
hone1er <josephmvillavicencio@gmail.com>
2020-03-05 11:31:48 -1000

    border

--- src/components/footer/technologies.js
+++ src/components/footer/technologies.js
@@ -13,7 +13,7 @@ export default class Tech extends Component {
                     <li><img className="icon" src={Python} alt="python"/><h4>python</h4></li>
                     <li><DiMongodb fill="#13aa52" className="icon" id="mongo" /><h4>mongodb</h4></li>
                     <li><DiHtml5 fill="tomato" className="icon" id="html-svg" /><h4>html5</h4></li>
-                    <li><DiSass fill="palevioletred" className="icon" id="sass-svg" /><h4>sass</h4></li>
+                    <li id="sass"><DiSass fill="palevioletred" className="icon" id="sass-svg" /><h4>sass</h4></li>
                 </ul>
             </div>
         )
