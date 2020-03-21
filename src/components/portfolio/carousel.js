09f9eb7df54c68eba925344a27d8e2d4aba952dd
hone1er <josephmvillavicencio@gmail.com>
2020-03-20 14:03:45 -1000

    changed portfolio section

--- src/components/portfolio/carousel.js
+++ src/components/portfolio/carousel.js
@@ -2,9 +2,10 @@ import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import './carousel.scss'
 import img1 from "./images/nameTrender4.JPG";
-import img2 from "./images/responsive.JPG";
-import img3 from "./images/thirsty.jpg";
+import img2 from "./images/responsive1.JPG";
+// import img3 from "./images/thirsty.jpg";
 import img4 from "./images/andres-urena-tsBropDpnwE-unsplash.jpg";
+import img5 from "./images/christopher-gower-m_HRfLhgABo-unsplash.jpg";
 // import img2 from "./images/undraw_progressive_app_m9ms.svg";
 // import img3 from "./images/undraw_feeling_proud_qne1.svg";
 
@@ -12,33 +13,40 @@ const service1 = {
   img: img1,
   title: "Name Trender",
   para:
-    "Enter a name and see how it has been trending in the U.S. since 1881!",
+    "Created as a personal project. Enter a name and see how it has been trending in the U.S. since 1881!",
     link: "https://www.nametrender.com"
 };
 const service2 = {
   img: img2,
   title: "Crony Creative",
   para:
-    "Implemented a responsive Adobe XD design ",
+    "Implemented a responsive Adobe XD design for client with HTML5/CSS3",
   link: "https://www.cronycreative.com"
 };
-const service3 = {
-  img: img3,
-  title: "Worldwide Water Analysis",
-  para:
-    "Created interactive graphs/maps with Ploty.js ",
-    link: "http://thirsty2thrive.world/"
-};
+// const service3 = {
+//   img: img3,
+//   title: "Worldwide Water Analysis",
+//   para:
+//     "Created interactive graphs/maps with Ploty.js ",
+//     link: "http://thirsty2thrive.world/"
+// };
 
 const service4 = {
   img: img4,
   title: "Amazon Alexa Skill",
   para:
-    "Implemented an Amazon Alexa skill in Python 3.6: ",
+    "Designed, implemented, & deployed an Amazon Alexa skill in Python 3.6",
     link: "https://www.amazon.com/Hone-Thrasher-video/dp/B07RLBPR8F"
 };
 
-const services = [service1, service2, service3, service4];
+const service5 = {
+  img: img5,
+  title: "Teacher Assistant",
+  para:
+    "Automated the process of transerring grade data between websites for a local teacher with Selenium",
+    link: "https://github.com/hone1er/teachersAid"
+};
+const services = [service1, service4,  service2, service5];
 
 function ControlledCarousel(props) {
   const [index, setIndex] = React.useState(0);
